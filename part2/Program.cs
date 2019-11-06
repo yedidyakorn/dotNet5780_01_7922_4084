956a48518b69113441a519fc0337489b23713d5c
yedidyakorn <48888380+yedidyakorn@users.noreply.github.com>
2019-11-06 09:37:12 +0200

    Update Program.cs

--- part2/Program.cs
+++ part2/Program.cs
@@ -18,9 +18,11 @@ namespace part2
                 for (int j = 0; j < arr.GetLength(1); j++)
                     arr[i, j] = false;
             }
+
             while (choice != 0)
             {
-                Console.WriteLine("Enter Choice: 1-enter new vication.\t 2-show whole list. \t 3-show taken dates.\t 0-exit");
+                Console.WriteLine("Enter Choice: 1-enter new vication.\t 2-show whole list. \t 3-show taken dates.\t 0-exit");//menu
+
                 choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
@@ -40,7 +42,6 @@ namespace part2
                         break;
                 }
             }
-
         }
 
         private static void newDate(bool[,] max)//checks if the date is avalible
@@ -53,20 +54,20 @@ namespace part2
             Console.WriteLine("Enter how many days:");
             duration = Convert.ToInt32(Console.ReadLine());
             bool[] arr = new bool[372];
-            maxToArry(max,arr);
-            begD = (month-1) * 31 + day-1;
-            endD = begD + duration;
-            if(month>12||month<1||day<1||day>31||endD>372||duration<2)//checks input
+            maxToArry(max,arr);//converts the calnder to one long array
+            begD = (month-1) * 31 + day-1;//begining day
+            endD = begD + duration;//ending day
+            if(month>12||month<1||day<1||day>31||endD>372||duration<2)// input check
             {
                 Console.WriteLine("ERROR - invalid input.");
                 return;
             }                       
-            if((arr[begD]==true&&arr[begD+1]==true)||(arr[endD]==true&&arr[endD-1]==true))
+            if((arr[begD]==true&&arr[begD+1]==true)||(arr[endD]==true&&arr[endD-1]==true))//checks if avalible
             {
                 Console.WriteLine("not avalible these dates");
                  return;
             }
-            for (int i=begD+1;i<endD-1;i++)
+            for (int i=begD+1;i<endD-1;i++)//checks if avalible
             {
                 if(arr[i]==true)
                 {
@@ -74,12 +75,12 @@ namespace part2
                     return;
                 }
             }
-            for (int i = begD; i < endD; i++)
+            for (int i = begD; i < endD; i++)//marks as taken
                 arr[i] = true;
-            arrayToMax(max,arr);
+            arrayToMax(max,arr);//converts back to calnder
             return;
             throw new NotImplementedException();
-            }
+        }
 
         private static void maxToArry(bool[,] max,bool[]arr)//convorts a array to matrix 
         {
@@ -110,10 +111,12 @@ namespace part2
             return;
             throw new NotImplementedException();
         }
-
+      
         private static void all(bool[,] arr)
-        {
+        { 
+
             int day = 1, month = 1;
+
             //flag for vacation period
             bool isCounting = false;
 
@@ -123,7 +126,7 @@ namespace part2
                 //set counting vaction period = true
                 day = day % 31 == 0 ? 31 : day % 31;
                 var currentDay = arr[month - 1, day - 1];
-                if (currentDay == true && !isCounting)
+                if (currentDay == true && !isCounting )
                 {
                     Console.Write("Start date : " + day + "/" + month);
                     isCounting = true;
@@ -131,8 +134,8 @@ namespace part2
                 //if vaction ended, isCounting = false
                 //or its the last day of the year
                 else if ((currentDay == false || (currentDay == true && day == 31 && month == 12)) && isCounting)
-                {
-                    Console.WriteLine(" , End date : " + (day - 1 == 0 ? 31 : day - 1) + "/" + ((day - 1 == 0) ? month - 1 : month));
+                {              
+                    Console.WriteLine(" , End date : " + (day -1 == 0 ? 31: day - 1) + "/" +  ((day-1 == 0) ? month-1:  month));
                     isCounting = false;
                 }
                 //increase day 
@@ -140,18 +143,17 @@ namespace part2
                 //calc if month should be increased
                 month += day / 32;
             }
-        }
-
-        private static void taken(bool[,] arr)//prints how many days are taken
+        }              
+        
+        private static void taken (bool[,] arr)//prints how many days are taken
         {
             int counter = 0;
             for (int i = 0; i < arr.GetLength(0); i++)
                 for (int j = 0; j < arr.GetLength(1); j++)
                     if (arr[i, j] == true)
                         counter++;     //Summarize the busy days
-            float percentageOfOccupancy = (((float)counter / 372) * 100);   //Percentage calculation
-            Console.WriteLine("The number of busy days per year:{0}\nPercentage of annual occupancy:{1}% ", counter, percentageOfOccupancy);
+            float percentageOfOccupancy = (((float)counter / 372)*100);   //Percentage calculation
+            Console.WriteLine("The number of busy days per year:{0}\nPercentage of annual occupancy:{1}% ", counter,  percentageOfOccupancy);
         }
-
     }
 }
